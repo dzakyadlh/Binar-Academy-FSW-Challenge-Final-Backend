2a3f06d578ae95bd6020f99872b3887e1b490c79
Agasss <agastya1203@gmail.com>
2023-02-15 21:05:43 +0700

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,13 +11,17 @@ const format = (User) => {
 }
 
 exports.register = (req, res) => {
+    try {
     user.register(req.body)
     .then((data) => {        
         res.json({ status:"Register Success", data: data});
     })
     .catch((err) => {
-        res.status(500).json({status: "Register Failed", msg: err})
+        res.status(409).json({status: "Register Failed", msg: err})
     })
+    } catch (err) {
+        res.status(500).json({status: "Register Failed", msg: err})
+    }
 }
 
 exports.login = (req, res) => {
@@ -27,7 +31,7 @@ exports.login = (req, res) => {
         res.json({ status:"Login Success", data: format(data)});
     })
     .catch((err) => {
-        res.status(400).json({status: "Login Failed", msg: err})
+        res.status(401).json({status: "Login Failed", msg: err})
     })
     } catch (err) {
         res.status(500).json({status: "Login Failed", msg: err.message})
@@ -35,38 +39,53 @@ exports.login = (req, res) => {
 }
 
 exports.showAll = (req, res) => {
+    try{
     user.findAll().then((data) => {
         res.json({status: "Successfully Show All Data", data: data});
     }).catch((err) => {
-        res.status(500).json({status: "Failed Show Data", msg: err});
+        res.status(400).json({status: "Failed Show Data", msg: err});
     })
+    } catch (err) {
+        res.status(500).json({status: "Failed Show Data", msg: err});
+    }
 }
 
 exports.findId = (req, res) => {
+    try{
     const {id} = req.params;
 
     user.findOne({where: { id: id }}).then((user) => {
         res.json({message: "User found", data: user});
     }).catch((err) => {
-        res.status(500).json({status: "Failed Show Data", msg: err});
+        res.status(400).json({status: "Failed Show Data", msg: err});
     })
+    } catch (err) {
+        res.status(500).json({status: "Failed Show Data", msg: err});
+    }
 }
 
 exports.forgotpassword = (req, res) => {
+    try {
     user.forgotpass(req.body).then(() => {
         res.json({message: "Update Success"});
     })
     .catch((err)=> {
-        res.json({message: "Update failed", msg: err});
+        res.status(400).json({message: "Update failed", msg: err});
     })
+    } catch (err) {
+        res.status(500).json({message: "Update failed", msg: err});
+    }
 }
 
 exports.deleteacc = (req, res) => {
+    try {
     user.deleteacc(req.body).then(() => {
         res.json({message: "Delete Success"});
     })
     .catch((err)=> {
-        console.log(err)
-        res.status(500).json({message: "Delete failed", msg: err});
+        res.status(400).json({message: "Delete failed", msg: err});
     })
+    } catch (err) {
+        res.status(500).json({message: "Delete failed", msg: err});
+    }
 }
