12c5c3f4d0624592990959ebbce464b30c2e6fff
Dzaky Adla <dzakyadlh@gmail.com>
2023-03-15 21:25:53 +0700

    revision

--- routes/games.js
+++ routes/games.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const games = require("../controllers/gamesController");
 
 router.get("/", games.get);
-router.get("/detail", games.getDetail);
+router.get("/:id", games.getDetail);
 router.post("/", games.createGame);
 router.put("/", games.updateGame);
 
